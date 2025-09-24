4044c608e96f7e5d1df039a4815d28b133b04a58
Alwyn Sterling <alwynsterling5@gmail.com>
2025-09-24 17:22:15 -0500

    Added win condition

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -19,6 +19,32 @@ document.addEventListener('DOMContentLoaded', () => {
                 currentPlayer = "X";
             }
             console.log(grid);
+
+            let winner = null;
+            const winningCombinations = [
+                [0, 1, 2],
+                [3, 4, 5],
+                [6, 7, 8],
+                [0, 3, 6],
+                [1, 4, 7],
+                [2, 5, 8],
+                [0, 4, 8],
+                [2, 4, 6]
+            ];
+
+            for (const combination of winningCombinations) {
+                const [a, b, c] = combination;
+                if (grid[a] && grid[a] === grid[b] && grid[a] === grid[c]) {
+                    winner = grid[a];
+                    break;
+                }
+            }
+
+            if (winner){
+                gameStatus = document.getElementById('status');
+                gameStatus.textContent = `Congratulations! ${winner} is the Winner!`;
+                gameStatus.classList.add('you-won');
+            }
         });
     }
 
