7446ba9fb095435bd58d4b5a73fb5aa029bec5fe
Alwyn Sterling <alwynsterling5@gmail.com>
2025-09-24 17:10:16 -0500

    Added X's and O's

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,6 +1,26 @@
 document.addEventListener('DOMContentLoaded', () => {
+    let grid = ["", "", "", "", "", "", "", "", ""];
+    let currentPlayer = "X";
+
     squares = document.getElementById('board').children;
     for (let i = 0; i < squares.length; i++) {
         squares[i].classList.add('square');
+        squares[i].addEventListener('click', () => {
+            if (currentPlayer === "X" && squares[i].textContent === '') {
+                grid[i] = 'X';
+                squares[i].textContent = "X";
+                squares[i].classList.add('X');
+                currentPlayer = "O";
+            }
+            else if (currentPlayer === "O" && squares[i].textContent === '') {
+                grid[i] = 'O';
+                squares[i].textContent = "O";
+                squares[i].classList.add('O');
+                currentPlayer = "X";
+            }
+            console.log(grid);
+        });
     }
+
+
 });
